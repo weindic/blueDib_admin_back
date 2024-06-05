5ebdb22b8dc865302b3d16176c826c12438b6a5d
shreyas-weindic <shreyaskumar0601@gmail.com>
2024-06-05 12:31:27 +0530

    Newsletter APIs

---
+++ route/newsletter.route.js
@@ -0,0 +1,29 @@
+const Router = require("express").Router();
+const newsletterController = require("../controller/newsletters.controller");
+
+Router.post(
+  "/v2/api/manage/newsletter/create",
+  newsletterController.createNewsletter
+);
+Router.post(
+  "/v2/api/manage/newsletter/getOne",
+  newsletterController.getNewsletterById
+);
+Router.post(
+  "/v2/api/manage/newsletter/getAll",
+  newsletterController.getAllNewsletter
+);
+Router.post(
+  "/v2/api/manage/newsletter/updateStatus",
+  newsletterController.updatesNewsletterStatus
+);
+Router.post(
+  "/v2/api/manage/newsletter/updateData",
+  newsletterController.updateData
+);
+Router.post("/v2/api/manage/newsletter/delete", newsletterController.delete);
+Router.post(
+  "/v2/api/manage/newsletter/send",
+  newsletterController.sendNewsletter
+);
+module.exports = Router;
