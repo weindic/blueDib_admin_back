6d315d5eb4fb1e9b11cc14bd79d8c84ceb946b45
weindic <166316868+weindic@users.noreply.github.com>
2024-06-08 00:39:56 +0530

    Update index.js
    ddd

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
-require("./connection/DB");
-require("dotenv").config();
 const multer = require("multer");
 const upload = multer();
+require("dotenv").config();
+require("./connection/DB");
 const authenticateJWT = require("./middleware/auth");
 
+// CORS Configuration
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : ["https://dashboard.bluedibs.com"];
 app.use(cors({
-  origin: "https://dashboard.bluedibs.com", // Replace with your actual Angular app URL
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
 
 // Other middleware configurations
 app.use(upload.any());
@@ -21,12 +28,12 @@ app.use(bodyParser.text({ type: "/" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Your existing route configurations
+// Route configurations
 app.use("", authenticateJWT, require("./route/adminData.route"));
 app.use("", authenticateJWT, require("./route/newsletter.route"));
 
 const PORT = process.env.PORT || 3000; // Use a default port if PORT is not defined
 
 app.listen(PORT, () => {
-  console.log(`Backend running on ${PORT}`);
+  console.log(`Backend running on port ${PORT}`);
 });
