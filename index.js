5e07ede085306768091bee37a203d12a6fa7be2c
weindic <166316868+weindic@users.noreply.github.com>
2024-06-08 01:30:56 +0530

    Update index.js
    cxkdsks

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const bodyParser = require("body-parser");
-const multer = require("multer");
+const bodyParser = require('body-parser');
+const multer = require('multer');
 const upload = multer();
-require("dotenv").config();
-require("./connection/DB");
-const authenticateJWT = require("./middleware/auth");
+const authenticateJWT = require('./middleware/auth');
 
-// CORS Configuration
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(",") : ["https://dashboard.bluedibs.com"];
+require('./connection/DB');
+require('dotenv').config();
+
+// Enable CORS for all routes
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: true,  // allows all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // allows all methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // allows specific headers
 }));
 
 // Other middleware configurations
 app.use(upload.any());
-app.use(bodyParser.text({ type: "/" }));
+app.use(bodyParser.text({ type: '/' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Route configurations
-app.use("", authenticateJWT, require("./route/adminData.route"));
-app.use("", authenticateJWT, require("./route/newsletter.route"));
+// Your existing route configurations
+app.use('', authenticateJWT, require('./route/adminData.route'));
 
 const PORT = process.env.PORT || 3000; // Use a default port if PORT is not defined
 
